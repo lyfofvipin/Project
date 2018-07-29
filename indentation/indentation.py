5702003a965addfe7266f72fc96704d14f42a73f
kumar vipin yadav <kumarvipinyadav369@gmail.com>
2018-07-30 00:19:33 +0530

    Update indentation.py

--- indentation/indentation.py
+++ indentation/indentation.py
@@ -1,5 +1,6 @@
 import sys
-f = open("/home/kumarvipinyadav/Documents/test.c","rt")
+f = open("File_path/File_name.c","rt")
+# you can enter file path with it's name
 flag ,z = 0,""
 for x in f.readlines():
     z = '    '*flag
@@ -12,3 +13,15 @@ for x in f.readlines():
             z = z + x[:x.index('{')+1] + '\n' + z + "  " + x[x.index('{')+1:-2] + '\n' + z + '}\n'
             x = ""
     print(z+x)
+    f.close()
+    
+'''Example :
+
+we have an example of C code here
+
+https://github.com/vipin3699/Projects/blob/master/indentation/test.c
+
+and we got It's output as shown here
+
+https://github.com/vipin3699/Projects/blob/master/indentation/Output
+'''
