0b3a5fe705ba688d20c34233e8f31dab31de1efc
Kumar Vipin Yadav <kumarvipinyadav369@gmail.com>
2019-05-22 21:56:35 +0530

    Created first View

--- MyBloger/Posts/views.py
+++ MyBloger/Posts/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views import Views
+from django.views import View
 
 class HomePage(View):
 
     def get(self, request, *args, **kwargs):
-        context = {'Helloworld':"Hellow world!","Title":"Title"}
-        return HttpResponse(context)
+        return HttpResponse("Home Page")
