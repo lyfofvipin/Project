36f21d9d7b0ee61fc5143796d5cad83add7ffff5
Kumar Vipin Yadav <kumarvipinyadav369@gmail.com>
2019-05-23 15:12:09 +0530

    Created Post Model

--- MyBloger/Posts/models.py
+++ MyBloger/Posts/models.py
@@ -2,4 +2,11 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
+    
     pass
+
+class Post(models.Model):
+
+    Auther = models.CharField(max_length=50)
+    Content = models.CharField(max_length=1000)
+    Publish_date = models.DateTimeField('pubdate')
