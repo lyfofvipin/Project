73238456fcef70a38c70a578685f181da6e8fa19
Kumar Vipin Yadav <kumarvipinyadav369@gmail.com>
2019-05-23 13:00:48 +0530

    App added to installed app

--- MyBloger/MyBloger/settings.py
+++ MyBloger/MyBloger/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'Posts.apps.PostsConfig'
+    'Posts.apps.PostsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
