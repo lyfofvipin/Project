9f925e0b60f94078c7b27b856d30e125662545c3
Kumar Vipin Yadav <kumarvipinyadav369@gmail.com>
2019-05-23 23:35:33 +0530

    Base Template and Home template is inited

--- MyBloger/Posts/views.py
+++ MyBloger/Posts/views.py
@@ -9,5 +9,5 @@ class HomePage(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["Title"] = "Home | Posts"
+        context["title"] = "Home | Posts"
         return context
