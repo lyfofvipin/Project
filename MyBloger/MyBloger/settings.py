b8f9c2b3a4e20ee00e8c728b8809fcc4c96ea7d0
Kumar Vipin Yadav <kumarvipinyadav369@gmail.com>
2019-05-26 17:10:56 +0530

    Regenrating Registration app

--- MyBloger/MyBloger/settings.py
+++ MyBloger/MyBloger/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'Posts.apps.PostsConfig',
-    'Registration.apps.RegistrationConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
