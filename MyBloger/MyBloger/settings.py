f2d1a7c128afb24633dad5dc099a7fdd82046865
Kumar Vipin Yadav <kumarvipinyadav369@gmail.com>
2019-05-27 19:10:52 +0530

    Addwd login and Logout fields

--- MyBloger/MyBloger/settings.py
+++ MyBloger/MyBloger/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'Loginapp.apps.LoginappConfig',
     'Posts.apps.PostsConfig',
     'Registrationapp.apps.RegistrationappConfig',
     'django.contrib.admin',
@@ -122,3 +123,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = 'Home'
