aa4d6cb5883862a91fa1a0764117547677053535
Kumar Vipin Yadav <kumarvipinyadav369@gmail.com>
2019-05-30 00:50:09 +0530

    User update solved

--- MyBloger/Profile/views.py
+++ MyBloger/Profile/views.py
@@ -1,12 +1,32 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.contrib.auth import get_user_model
+from .models import Profile
+from .forms import UserDetailUpdateForm, UserProfileUpdateForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic.edit import UpdateView
 
-class Profile(TemplateView):
+User = get_user_model()
 
-    template_name = 'Profile/Profile.html'
+
+class Profile(LoginRequiredMixin, UpdateView):
+
+    model = User
+    form_class = UserDetailUpdateForm
+    # second_form_class = UserProfileUpdateForm
+    template_name = "Profile/Profile.html"
+    success_url = "/profile"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["title"] = "Profile | Post"
+        # context['pro_form'] = self.second_form_class(self.request.POST, instance=self.object.user)
+        context['title'] = 'Profile | Post'
         return context
-    
+
+    # def form_valid(self, form):
+    #     pro_form = UserProfileUpdateForm(self.request.POST, instance=self.object.user)
+    #     if pro_form.is_valid(form):
+    #         pro_form.save()
+    #     return super().form_valid()
+
+    def get_object(self):
+        return self.request.user
