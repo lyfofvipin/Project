4fa8bd636d39adc7809478973b16f855d9ded6bf
= <=>
2019-06-05 12:35:19 +0530

    Adding Template

--- MyBloger/Profile/models.py
+++ MyBloger/Profile/models.py
@@ -12,12 +12,12 @@ class Profile(models.Model):
     def  __str__(self):
         return self.user.username
 
-    def save(self, *args, **kwargs):
-        super().save(*args, **kwargs)
+    # def save(self, *args, **kwargs):
+    #     super().save(*args, **kwargs)
 
-        img = Image.open(self.Profile_pic.path)
+    #     img = Image.open(self.Profile_pic.path)
 
-        if img.height > 300 and img.width > 300:
-            output_size = (300,300)
-            img.thumbnail(output_size)
-            img.save(self.Profile_pic.path)
+    #     if img.height > 300 and img.width > 300:
+    #         output_size = (300,300)
+    #         img.thumbnail(output_size)
+    #         img.save(self.Profile_pic.path)
