416da8aae349796b1e904df2c874203a32d5559d
= <=>
2019-06-07 14:47:11 +0530

    Project second Last step to complete

--- MyBloger/MyBloger/settings.py
+++ MyBloger/MyBloger/settings.py
@@ -139,5 +139,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'kumarvipinyadav369@gmail.com'
+EMAIL_HOST_USER = ''
+#Put your gmail address above ^^
 EMAIL_HOST_PASSWORD = ''
+#Put your gmail Password above ^^
