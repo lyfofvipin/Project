25dadfd3200b4545b434e17bcc8513b2c0b6d18f
Kumar Vipin Yadav <kumarvipinyadav369@gmail.com>
2019-07-04 21:34:04 +0530

    testing

--- testingjen.py
+++ testingjen.py
@@ -1,3 +1,3 @@
 print("This is a code for testing jenkins")
 print("This is after changing or adding it on github")
-print("This is after changing or adding it on github")
+print("This is after changing or adding it on github")print(" Wrong statement)
