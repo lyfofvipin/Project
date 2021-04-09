f36e976d1d1131bf87c9a86869a2a4f5dffd84a6
Vipin Kumar <vipikuma@redhat.com>
2021-04-09 13:51:49 +0530

    Removed MyBloger from the Project dir.

--- MyBloger/Profile/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-
-class ProfileConfig(AppConfig):
-    name = 'Profile'
-
-    def ready(self):
-        import Profile.signels 
--- MyBloger/Registrationapp/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import Registration
-
-urlpatterns = [
-    path('register', Registration.as_view(), name='Registration')
-]
